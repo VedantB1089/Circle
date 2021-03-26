a0bc0b3d6d859355809ddb940c6af084ca0bfc3b
VedantB1089 <77881640+VedantB1089@users.noreply.github.com>
2021-03-26 14:38:54 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,18 @@
+canvas = document.getElementById("myCanvas");
+color = "orange";
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(368,437,40,0,2 * Math.PI);
+ctx.stroke();
+    canvas.addEventListener("mousedown", my_mousedown);
+    function my_mousedown(e)
+    {
+        color = document.getElementById("color").value;
+        console.log(color);
+        mouse_x = e.clientX - canvas.offsetLeft;
+        mouse_y = e.clientY - canvas.offsetTop;
+        console.log("X = " + mouse_x +" ,Y = " + mouse_y);
+        circle(mouse_x , mouse_y);
+    }
